27906a6e299c8773b55c341da13f13a9f504ae39
AngoLxury <angoluxury@gmail.com>
2024-12-13 23:01:04 +0100

    vamos ver agr
    não falha

---
+++ game.js
@@ -0,0 +1,4 @@
+document.querySelector("#newsletter").addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Obrigado por se inscrever na nossa newsletter! Fique atento às novidades.");
+});
